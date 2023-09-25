977c73f4aeecb705e35d15fc3cf28e5daaabd248
mercedesgarnham <47762248+mercedesgarnham@users.noreply.github.com>
2023-09-25 20:06:19 -0300

    Fix pipeline and functions

--- pipeline_functions.py
+++ pipeline_functions.py
@@ -1,40 +1,39 @@
 #pipeline_functions
-import pandas 
+import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import Descriptors
 
-def leer_tabla_columnas(nombre_archivo,sep,columnas=0):
+def read_dataframe_columns(file_name,sep,columns=0):
     '''
-    Lee una tabla de datos en csv y devuelve un dataframe 
-    que contiene las columnas indicadas como parametro en formato lista
+    Read a data table from a CSV file and return a dataframe 
+    containing the columns specified as a parameter in list format
     '''
-    data_raw = pandas.read_csv(nombre_archivo,sep=sep)
-    if columnas==0:
+    data_raw = pd.read_csv(file_name,sep=sep)
+    if columns==0:
         data_filtered = data_raw
     else: 
-        data_filtered = data_raw[columnas]
+        data_filtered = data_raw[columns]
     return (data_filtered)
 
 
-def filtrar_datos(df,columna,criterio):
+def data_filtering(df,column,criteria):
     '''
-    Recibe un dataframe y un criterio de filtrado, 
-    devolviendo el df solo con los parametros que coinciden con el filtro
-    '''
-    columnas = df.columns
-    for i in range(len(columnas)):
-        is_criterio = df[columna] == criterio
-        df_filtered = df[is_criterio]
+    Receives a dataframe and a filtering criteria, 
+    returns the dataframe only with the parameters that match the criteria.    '''
+    columns = df.columns
+    for i in range(len(columns)):
+        is_criteria = df[column] == criteria
+        df_filtered = df[is_criteria]
     return (df_filtered)
 
 
-def interseccion_datos(data,selection_data,columna_df,columna_selection_data,filter_name,data_type):
+def data_intersection(data,selection_data,column_df,column_selection_data,filter_name,data_type):
     '''
-    Recibe dos dataframes, uno con los datos a analizar y otro para seleccionar, y las columnas con las que se va a realizar el filtrado
-    devolviendo el df filtrado
+    Receives two dataframes, one with the data to analyze and another to select, 
+    and the columns to use as filtering criteria returning the filtered df
     '''
-    df = data[columna_df].unique().tolist()
-    df_filtering = selection_data[columna_selection_data].unique().tolist()
+    df = data[column_df].unique().tolist()
+    df_filtering = selection_data[column_selection_data].unique().tolist()
     intersected_column = []
     not_intersected_column = []
     filter_name_column = []
@@ -52,67 +51,80 @@ def interseccion_datos(data,selection_data,columna_df,columna_selection_data,fil
             filter_name_column.append('N/A')
             pass
             
-    intersected_data_raw = pandas.DataFrame(columns=[data_type])
+    intersected_data_raw = pd.DataFrame(columns=[data_type])
     intersected_data_raw[data_type] = intersected_column
     
-    not_intersected_data_raw = pandas.DataFrame(columns=[data_type, 'Filter Name'])
+    not_intersected_data_raw = pd.DataFrame(columns=[data_type, 'Filter Name'])
     not_intersected_data_raw[data_type] = not_intersected_column
     not_intersected_data_raw['Filter Name'] = filter_name_column
     
-    pre_intersected_data = pandas.merge(left=data,right=intersected_data_raw,how="inner",left_on=data_type, right_on=data_type)
-    intersected_data = pandas.merge(left=selection_data,right=pre_intersected_data,how="inner",left_on=columna_selection_data, right_on=data_type)
+    pre_intersected_data = pd.merge(left=data,right=intersected_data_raw,how="inner",left_on=data_type, right_on=data_type)
+    intersected_data = pd.merge(left=selection_data,right=pre_intersected_data,how="inner",left_on=column_selection_data, right_on=data_type)
    
     return (intersected_data, not_intersected_data_raw)
 
+def data_not_in_intersection(data,selection_data,column_df,column_selection_data,filter_name):
+    '''
+    The function is designed to filter a DataFrame (data) by excluding rows 
+    that have matching values in a specified column of the DataFrame 
+    with a selection DataFrame (selection_data). 
+    '''
+    df_novel= pd.DataFrame()
+    df_tested= pd.DataFrame()
+    df_resultado = data.merge(selection_data, left_on=column_df, right_on=column_selection_data, how='left', indicator=True)
+    df_resultado = df_resultado.drop(columns=['Unnamed: 0', 'smiles', 'mol', 'inchi', 'inchikey'])
+    df_novel = df_resultado[df_resultado['_merge'] == 'left_only']
+    df_tested = df_resultado[df_resultado['_merge'] == 'both']
+    df_novel = df_novel.drop(columns=['_merge'])
+    df_tested = df_tested.drop(columns=['_merge'])
+    df_tested['Filter Name'] = filter_name
+    return (df_novel, df_tested)
 
-def resumen_contar_datos(df,columna):
+def resumen_contar_datos(df,column):
     '''
-    Recibe un dataframe y un criterio de filtrado, 
-    devolviendo una tabla con el conteo de cada parametro
+    Receives a dataframe and a filter criterion,
+    returning a table with the count of each parameter
     '''
-    columnas = df.columns
-    for i in range(len(columnas)):
-        n_datos = df[columnas[i]].value_counts()
+    columns = df.columns
+    for i in range(len(columns)):
+        n_datos = df[columns[i]].value_counts()
         print(n_datos)
     return (print(n_datos))
      
    
-def smiles_to_inchikey(df,compound_columna):
+def smiles_to_inchikey(df,compound_column):
     '''
-    Recibe un dataframe y la columna donde se encuentran los smiles de los compuestos, 
-    devolviendo el mismo df con una columna nueva, con los compuestos en formato inchikey
-    '''
-    pandas.options.mode.chained_assignment = None
-    gdi_aviable_novel_drug_like_analysis = df
+    Receives a dataframe and the column where the smiles of the compounds are located,
+    returning the same df with a new column, with the compounds in inchikey format    '''
+    pd.options.mode.chained_assignment = None
     mol_tested_list= []
-    for element in df[compound_columna]:
+    for element in df[compound_column]:
         mol_tested = Chem.MolFromSmiles(element)
         mol_tested_list.append(mol_tested)
-    gdi_aviable_novel_drug_like_analysis['mol'] = mol_tested_list
+    df['mol'] = mol_tested_list
     inchikey_list = []
-    for element in gdi_aviable_novel_drug_like_analysis['mol']:
+    for element in df['mol']:
         try:
             inchikey = Chem.MolToInchiKey(element)
             inchikey_list.append(inchikey)
         except:
             inchikey_list.append('N/A')
             pass
-    gdi_aviable_novel_drug_like_analysis['inchiKey'] = inchikey_list
-    return(gdi_aviable_novel_drug_like_analysis)
+    df['inchiKey'] = inchikey_list
+    return(df)
 
-def drug_likness(df,compound_columna):
-    pandas.options.mode.chained_assignment = None
-    gdi_aviable_novel_drug_like_analysis = df
+def drug_likness(df,compound_column):
+    pd.options.mode.chained_assignment = None
     mol_tested_list= []
-    for element in df[compound_columna]:
+    for element in df[compound_column]:
         mol_tested = Chem.MolFromSmiles(element)
         mol_tested_list.append(mol_tested)
-    gdi_aviable_novel_drug_like_analysis['mol'] = mol_tested_list
+    df['mol'] = mol_tested_list
     NumHDonors_list = []
     NumHAcceptors_list = []
     MW_list = []
     LogP_list = []
-    for element in gdi_aviable_novel_drug_like_analysis['mol']:
+    for element in df['mol']:
         try:
             NumHDonors = Descriptors.NumHDonors(element)
             NumHDonors_list.append(NumHDonors)
@@ -137,28 +149,24 @@ def drug_likness(df,compound_columna):
         except:
             LogP_list.append('N/A')
             pass
-    gdi_aviable_novel_drug_like_analysis['NumHDonors'] = NumHDonors_list
-    gdi_aviable_novel_drug_like_analysis['NumHAcceptors'] = NumHAcceptors_list
-    gdi_aviable_novel_drug_like_analysis['MW'] = MW_list
-    gdi_aviable_novel_drug_like_analysis['logP'] = LogP_list
+    df['NumHDonors'] = NumHDonors_list
+    df['NumHAcceptors'] = NumHAcceptors_list
+    df['MW'] = MW_list
+    df['logP'] = LogP_list
     #Cuento cuantas caracterísicas cumple cada compuesto
     countLipinski = lambda row: int(row['NumHDonors'] < 6) +  int(row['NumHAcceptors'] < 6) + int(row['MW'] < 500) + int(row['logP'] < 6)
-    gdi_aviable_novel_drug_like_analysis['Lipinski'] = gdi_aviable_novel_drug_like_analysis.apply(countLipinski,axis=1)
+    df['Lipinski'] = df.apply(countLipinski,axis=1)
     countRO3 = lambda row: int(row['NumHDonors']<= 3) +  int(row['NumHAcceptors']<= 3) + int(row['MW']< 300) + int(row['logP']<= 3)
-    gdi_aviable_novel_drug_like_analysis['RO3'] = gdi_aviable_novel_drug_like_analysis.apply(countRO3,axis=1)
+    df['RO3'] = df.apply(countRO3,axis=1)
     #Selecciono aquellos compuestos que cumplen con 3 o más
-    gdi_aviable_novel_druglike_lipinski = gdi_aviable_novel_drug_like_analysis[gdi_aviable_novel_drug_like_analysis['Lipinski']>2]
-    gdi_aviable_novel_druglike_RO3 = gdi_aviable_novel_drug_like_analysis[gdi_aviable_novel_drug_like_analysis['RO3']>2]
-    gdi_aviable_novel_druglike_not_lipinski = gdi_aviable_novel_drug_like_analysis[gdi_aviable_novel_drug_like_analysis['Lipinski']<3]
-    gdi_aviable_novel_druglike_not_RO3 = gdi_aviable_novel_drug_like_analysis[gdi_aviable_novel_drug_like_analysis['RO3']<3]
+    gdi_aviable_novel_druglike_lipinski = df[df['Lipinski']>2]
+    gdi_aviable_novel_druglike_RO3 = df[df['RO3']>2]
+    gdi_aviable_novel_druglike_not_lipinski = df[df['Lipinski']<3]
+    gdi_aviable_novel_druglike_not_RO3 = df[df['RO3']<3]
     #Filtrado según drug like
-    df_druglike = pandas.concat([gdi_aviable_novel_druglike_lipinski,gdi_aviable_novel_druglike_RO3])
-    df_not_druglike = pandas.concat([gdi_aviable_novel_druglike_not_lipinski,gdi_aviable_novel_druglike_not_RO3])
+    df_druglike = pd.concat([gdi_aviable_novel_druglike_lipinski,gdi_aviable_novel_druglike_RO3])
+    df_not_druglike = pd.concat([gdi_aviable_novel_druglike_not_lipinski,gdi_aviable_novel_druglike_not_RO3])
     df_not_druglike['Filter Name'] = ["Not Druglike"] * len(df_not_druglike)
     return(df_druglike, df_not_druglike)
 
-def generar_csv_filtrado(df,columna_df,criterio):
-    datos_filtrados = filtrar_datos(df,columna_df,criterio)
-    file_name = 'out_file.csv'
-    datos_filtrados.to_csv(file_name)
     
